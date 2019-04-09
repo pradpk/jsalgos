c1eccf8fbffd8f1314e70c77f2065f268811be9a
pradpk <Pradhip.nit@gmail.com>
2019-04-09 11:01:58 -0400

    Add files via upload

---
+++ quicksort.js
@@ -0,0 +1,33 @@
+let a = [100,32,67,135,245,22,23];
+console.log("Before starting--" + a);
+console.log(quicksort(a, 0, a.length-1));
+
+function quicksort(a, lo, hi) {
+	if(lo < hi) {
+		p = partition(a, lo, hi);
+		quicksort(a, lo, p-1);
+		quicksort(a, p+1, hi);
+	}
+	return a;
+}
+
+function partition(a, lo, hi) {
+	pivot = a[hi];
+	i = lo;
+	for(j=lo;j<hi;j++) {
+		if(a[j] < pivot) {
+			tmp = a[i];
+			a[i] = a[j];
+			a[j] = tmp;
+			i=i+1;
+		}
+	}	
+	tmp = a[i];
+	a[i] = a[hi];
+	a[hi] = tmp;
+	console.log("Step -> " + a +  "piv: " + i);
+	return i;
+}
+
+
+
